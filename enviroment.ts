eeb556df682321cbad1ea76569fddfd97d666ed7
ttsukuyomi3auka <kostiakarpenok@gmail.com>
2024-11-26 15:46:28 +0500

    ginignore settings

---
+++ enviroment.ts
@@ -0,0 +1,11 @@
+import * as dotenv from "dotenv";
+dotenv.config({ path: ".env.dev" });
+import { cleanEnv, num, str } from "envalid";
+
+export const Enviroment = cleanEnv(process.env, {
+  PORT: num({ desc: "sever port" }),
+  MONGO_ROOT_USERNAME: str({ desc: "mongo user" }),
+  MONGO_ROOT_PASSWORD: str({ desc: "mongo password" }),
+  MONGO_PORT: str({ desc: "mongo docker port" }),
+  NODE_ENV: str({ choices: ["dev", "test", "production"] }),
+});
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,22 @@
 import express from "express";
+import { Enviroment } from "../enviroment";
+import mongoose from "mongoose";
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+Enviroment.PORT;
+
+const mongoString = `mongodb://${Enviroment.MONGO_ROOT_USERNAME}:${Enviroment.MONGO_ROOT_PASSWORD}@${Enviroment.MONGO_PORT}/schedule?authSource=admin`;
 
 app.get("/", (req, res) => {
   res.send("Hello, Express!");
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(Enviroment.PORT, async () => {
+  const mongooseConnect = await mongoose.connect(mongoString);
+  if (mongooseConnect.connection.readyState == mongooseConnect.STATES.connected) {
+    console.log(`👍 Mongoose successfull connection!`);
+  } else {
+    console.log(`🤬 Mongoose connection is failed!`);
+  }
+  console.log(`Server is running on http://localhost:${Enviroment.PORT}`);
 });
